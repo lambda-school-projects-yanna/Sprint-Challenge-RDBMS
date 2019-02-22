f60215013e6fb6730c067c8d5c086ade8cde0a43
yannafaith <yannafaith97@gmail.com>
2019-02-22 10:46:09 -0700

    finishes stretch endpoints

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ server.get('/api', (req, res) => {
     res.send('API Running.')
 });
 
-// ========== Projects Endpoints
+// ========== Projects Endpoints =========== \\
+
+// GET
 
 server.get('/api/projects', (req, res) => {
     db('projects')
@@ -62,49 +64,117 @@ server.get('/api/projects/:id', (req, res) => {
       });
   });
 
-  server.post('/api/projects', (req, res) => {
-      const project = req.body;
-      if (!project.hasOwnProperty('name') || !project.hasOwnProperty('complete')) {
-          res.status(400).json({error: 'Please add project properties <name> and <complete>.'})
-      } else {
-      db('projects').insert(project)
-      .then(project => {
-          res.status(201).json({id : project[0]})
-      })
-      .catch(() => {
-          res.status(500).json({error: 'The project could not be created.'})
-      })
-    }
-  });
+// POST
 
-// ========== Actions Endpoints
-
-  server.get('/api/actions', (req, res) => {
-      db('actions')
-      .then(actions => {
-          res.status(200).json(actions)
-      })
-      .catch(() => {
-          res.status(500).json({error: 'Unable to retrieve actions.'})
-      })
-  });
-
-  server.post('/api/actions', (req, res) => {
-      const action = req.body;
-      if (!action.hasOwnProperty('complete') || !action.hasOwnProperty('project_id')) {
-          res.status(400).json({error: "Please add properties <complete> and <project_id> to the action"})
-      } else {
-      db('actions').insert(action)
-      .then(project => {
+server.post('/api/projects', (req, res) => {
+    const project = req.body;
+    if (!project.hasOwnProperty('name') || !project.hasOwnProperty('complete')) {
+        res.status(400).json({error: 'Please add project properties <name> and <complete>.'})
+    } else {
+    db('projects').insert(project)
+    .then(project => {
         res.status(201).json({id : project[0]})
-      })
-      .catch(() => {
-          res.status(500).json({error: 'The action could not be created.'})
-      })
-    }
-  });
+    })
+    .catch(() => {
+        res.status(500).json({error: 'The project could not be created.'})
+    })
+}
+});
 
-  
+// DELETE 
+
+server.delete('/api/projects/:id', (req, res) => {
+    const id = req.params.id;
+    db('projects').where({'projects.id' : id}).del()
+    .then(() => {
+        res.status(200).json({message: `Deleted project with id:${req.params.id}`})
+    })
+    .catch(() => {
+        res.status(500).json({error: "Project could not be deleted."})
+    })
+});
+
+// PUT
+
+server.put('/api/projects/:id', (req, res) => {
+    const update = req.body;
+    db('projects').where({'projects.id' : req.params.id}).update(update)
+    .then(() => {
+        res.status(200).json({message: `Updated project with id:${req.params.id}`})
+    })
+    .catch(() => {
+        res.status(500).json({error: "Could not update project."})
+    })
+});
+
+// ========== Actions Endpoints =========== \\
+
+// GET 
+
+server.get('/api/actions', (req, res) => {
+    db('actions')
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch(() => {
+        res.status(500).json({error: 'Unable to retrieve actions.'})
+    })
+});
+
+server.get('/api/actions/:id', (req, res) => {
+    db('actions').where({'actions.id' : req.params.id})
+    .then(action => {
+        res.status(200).json(action)
+    })
+    .catch(() => {
+        res.status(500).json({error: 'Unable to retrieve action.'})
+    })
+});
+
+// POST
+
+server.post('/api/actions', (req, res) => {
+    const action = req.body;
+    if (!action.hasOwnProperty('complete') || !action.hasOwnProperty('project_id')) {
+        res.status(400).json({error: "Please add properties <complete> and <project_id> to the action"})
+    } else {
+    db('actions').insert(action)
+    .then(project => {
+    res.status(201).json({id : project[0]})
+    })
+    .catch(() => {
+        res.status(500).json({error: 'The action could not be created.'})
+    })
+}
+});
+
+// DELETE 
+
+server.delete('/api/actions/:id', (req, res) => {
+    const id = req.params.id;
+    db('actions').where({'actions.id' : id}).del()
+    .then(() => {
+        res.status(200).json({message: `Deleted action with id:${req.params.id}`})
+    })
+    .catch(() => {
+        res.status(500).json({error: "Action could not be deleted."})
+    })
+});
+
+// PUT
+
+server.put('/api/actions/:id', (req, res) => {
+    const update = req.body;
+    db('actions').where({'actions.id' : req.params.id}).update(update)
+    .then(() => {
+        res.status(200).json({message: `Updated action with id:${req.params.id}`})
+    })
+    .catch(() => {
+        res.status(500).json({error: "Could not update action."})
+    })
+});
+
+
 const port = process.env.PORT || 4000;
 server.listen(port, () => console.log(`\n *** server running on port ${port} *** \n`));
 
